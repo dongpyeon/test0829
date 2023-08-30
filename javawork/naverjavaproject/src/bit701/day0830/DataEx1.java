e65de444a96e0b7148f3447055f42852a528c03d
dong <kd0948@gmail.com>
2023-08-30 13:26:11 +0900

    2일차테스트1

---
+++ javawork/naverjavaproject/src/bit701/day0830/DataEx1.java
@@ -0,0 +1,13 @@
+package bit701.day0830;
+
+public class DataEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//한글 
+		System.out.println("hello!11");
+		System.out.println("happy!2232");
+		
+	}
+
+}
